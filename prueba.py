7e49f6877bd2fbf1a1e5de446641f7d075c162bf
Juan Carlos Campos <131291609+JCCT73@users.noreply.github.com>
2023-10-16 07:30:55 +0200

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -19,7 +19,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import auc
 import warnings
 warnings.filterwarnings("ignore")
-p
+
 st.title("Predicción de la Intención de compra de los compradores en línea por Internet de comercio electrónico")
 
 uploaded_file = st.file_uploader("Cargar el archivo CSV", type=["csv"])
