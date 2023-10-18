bccdecb7b20de8cbd79f5d79e229735eb8a131f6
Juan Carlos Campos <131291609+JCCT73@users.noreply.github.com>
2023-10-18 20:55:32 +0200

    Add files via upload

--- custom_functions.py
+++ custom_functions.py
@@ -1,6 +1,26 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
+import pandas as pd
+import numpy as np
+import joblib
+import pickle
+import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 
 def eliminar_columna(dataframe, columna):
