e4166dc4f49622b946d92557e31e3b4217415f01
Juan Carlos Campos <131291609+JCCT73@users.noreply.github.com>
2023-10-18 08:02:46 +0200

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -2,13 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-import pickle
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import RandomForestClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -20,65 +17,66 @@ from sklearn.metrics import auc
 import warnings
 warnings.filterwarnings("ignore")
 
-st.title("Predicción de compra por Internet")
+from utils.custom_functions import estandarizar_columnas
 
-uploaded_file = st.file_uploader("Cargar el archivo CSV", type=["csv"])
+model = joblib.load(r'C:\Users\axa\THE BRIDGE_23\SEMANA 24. CORE. PROYECTO ML\81. PROYECTO ML\src\model\my_model.joblib')
 
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
+def hacer_prediccion(valores_de_entrada):
+    entrada_array = np.array(valores_de_entrada).reshape(1, -1)
+    prediccion = model.predict(entrada_array)
+    return prediccion
 
-    st.write('**Datos Cargados:**')
-    st.write(data)
 
-    # Utilizar el LabelEncoder para transformar la columna "Weekend"
-    column_to_encode = 'Weekend'
-    le = LabelEncoder()
-    data[column_to_encode] = le.fit_transform(data[column_to_encode])
+st.title('Predicción de compras por Internet')
 
-    st.write('**Datos con la columna "Weekend" transformada:**')
-    st.write(data)
+administrative = st.number_input('Administrative', value=0)
+administrative_duration = st.number_input('Administrative Duration', value=0.0)
+informational = st.number_input('Informational', value=0)
+informational_duration = st.number_input('Informational Duration', value=0.0)
+product_related = st.number_input('Product Related', value=0)
+product_related_duration = st.number_input('Product Related Duration', value=0.0)
+bounce_rates = st.number_input('Bounce Rates', value=0.0)
+exit_rates = st.number_input('Exit Rates', value=0.0)
+page_values = st.number_input('Page Values', value=0.0)
+special_day = st.number_input('Special Day', value=0.0)
+operating_systems = st.number_input('Operating Systems', value=0)
+browser = st.number_input('Browser', value=0)
+region = st.number_input('Region', value=0)
+traffic_type = st.number_input('Traffic Type', value=0)
+weekend = st.checkbox('Weekend')
+visitor_type_other = st.checkbox('Visitor Type - Other')
+visitor_type_returning_visitor = st.checkbox('Visitor Type - Returning Visitor')
 
-    # Utilizar el LabelEncoder para transformar la columna "Revenue"
-    column_to_encode = 'Revenue'
-    le = LabelEncoder()
-    data[column_to_encode] = le.fit_transform(data[column_to_encode])
+if st.button('Realizar Predicción'):
+    user_input = pd.DataFrame({
+        'Administrative': [administrative],
+        'Administrative_Duration': [administrative_duration],
+        'Informational': [informational],
+        'Informational_Duration': [informational_duration],
+        'ProductRelated': [product_related],
+        'ProductRelated_Duration': [product_related_duration],
+        'BounceRates': [bounce_rates],
+        'ExitRates': [exit_rates],
+        'PageValues': [page_values],
+        'SpecialDay': [special_day],
+        'OperatingSystems': [operating_systems],
+        'Browser': [browser],
+        'Region': [region],
+        'TrafficType': [traffic_type],
+        'Weekend': [weekend],
+        'VisitorType_Other': [visitor_type_other],
+        'VisitorType_Returning_Visitor': [visitor_type_returning_visitor]
+    })
 
-    st.write('**Datos con la columna "Revenue" transformada:**')
-    st.write(data)
-
-    # Transformar la columna "Month" en dummies
-    data = pd.get_dummies(data, columns=['Month'], prefix='Month', drop_first=True)
-
-    # Transformar la columna "VisitorType" en dummies
-    data = pd.get_dummies(data, columns=['VisitorType'], prefix='VisitorType', drop_first=True)
-
-    st.write('**Datos transformados con variables dummies:**')
-
-    # Escalar las características numéricas
-    caracteristicas_a_escalar = ['Administrative', 'Administrative_Duration', 'Informational', 'Informational_Duration',
-                                'ProductRelated', 'ProductRelated_Duration', 'BounceRates', 'ExitRates', 'PageValues', 'SpecialDay',
-                                'OperatingSystems', 'Browser', 'Region', 'TrafficType', 'Weekend']
-
-    caracteristicas_numericas = data[caracteristicas_a_escalar]
-    sc = StandardScaler()
-    escaladas = sc.fit_transform(caracteristicas_numericas)
-    data[caracteristicas_a_escalar] = escaladas
-
-    st.write('**Datos con características numéricas escaladas:**')
-    st.write(data)
-
-    loaded_model = joblib.load('my_model.pkl')
-
-    # Solicitar nuevos datos al usuario o cargarlos desde alguna fuente
-    nuevos_datos = obtener_nuevos_datos()  # Reemplaza esto con tu método de obtención de datos
-
-    # Asegúrate de que los nuevos datos tengan el mismo formato y características que los datos de entrenamiento
-
-    # Utiliza el modelo cargado para hacer una predicción en los nuevos datos
-    prediccion = loaded_model.predict(nuevos_datos)
-
-    # La variable 'prediccion' contendrá el resultado de la predicción (comprará o no comprará)
+    columnas_numericas = ['Administrative', 'Administrative_Duration', 'Informational',
+                          'Informational_Duration', 'ProductRelated', 'ProductRelated_Duration',
+                          'BounceRates', 'ExitRates', 'PageValues', 'SpecialDay',
+                          'OperatingSystems', 'Browser', 'Region', 'TrafficType']
+    
    
+    user_input = estandarizar_columnas(user_input, columnas_numericas)
 
+    # Realiza la predicción
+    prediction = model.predict(user_input)
 
-
+    st.write(f'La predicción es: {prediction[0]}')
